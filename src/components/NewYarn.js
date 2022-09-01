2a84f0d8f4e9405dbac7b3fff501d824e9bd2a6f
Roma Joy <joynormahouser@gmail.com>
2022-09-01 09:19:33 -0500

    final touchups, placeholders, finishings

--- src/components/NewYarn.js
+++ src/components/NewYarn.js
@@ -7,29 +7,29 @@ const NewYarn = (props) => {
       <h1>Add Yarn to Stash</h1>
       <form className="row mt-3" onSubmit={props.handleNewYarnSubmit}>
         <div className="col-md-6">
-            <label className="form-label">Yarn Brand</label>
-            <input className="form-control" type="text" onChange={props.handleYarnBrand}/>
+            
+            <input className="form-control" type="text" onChange={props.handleYarnBrand} placeholder="Yarn Brand" />
         </div>
         <div className="col-md-6">
-            <label className="form-label">Name</label>
-            <input className="form-control" type="text" onChange={props.handleYarnName}/>
+            
+            <input className="form-control" type="text" onChange={props.handleYarnName} placeholder="Yarn Name"/>
         </div>
         <div className="col-md-6">
-            <label className="form-label">Color of Yarn</label>
-            <input className="form-control" type="text" onChange={props.handleYarnColor}/>
+            <label></label>
+            <input className="form-control" type="text" onChange={props.handleYarnColor} placeholder="Color"/>
         </div>
         <div className="col-md-6">
-            <label className="form-label">No. of Yards</label>
-            <input className="form-control" type="number" onChange={props.handleYarnYards}/>
+            <label className="form-label"></label>
+            <input className="form-control" type="number" onChange={props.handleYarnYards} placeholder="Yardage per skein"/>
         </div>
         <div className="col-md-6">
-            <label className="form-label">Yarn Material</label>
-            <input className="form-control" type="text" onChange={props.handleYarnFiber}/>
+            <label className="form-label"></label>
+            <input className="form-control" type="text" onChange={props.handleYarnFiber} placeholder="Fiber"/>
         </div>
         <div className="col-md-6">
-            <label className="form-label">Yarn Weight</label>
+            <label className="form-label"></label>
             <select className="form-select" onChange={props.handleYarnWeight}>
-                <option ></option>
+                <option disabled>Yarn Weight</option>
                 <option >Lace</option>
                 <option >Sock, Fingering, Super Fine</option>
                 <option >Sport, Baby</option>
@@ -39,27 +39,29 @@ const NewYarn = (props) => {
                 <option >Super Bulky</option>
                 <option >Jumbo, Roving</option>
             </select>
-        </div>
+        </div>      
         <div className="col-md-6">
-            <label className="form-label">Date of Purchase</label>
+            <label></label>
+            <input className="form-control" type="number" onChange={props.handleYarnSkeins} placeholder="Number of Skeins" />
+        </div>
+         <div className="col-md-6">
+            <label></label>
+            <input className="form-control" type="url" onChange={props.handleYarnImage} placeholder="Image URL"/>
+        </div>  
+
+        <div className="col-md-6 mt-4  input-group">
+           <input className="form-control" type="text" onChange={props.handleYarnStore} placeholder="Yarn Store"/>
+           <span className="input-group-text ">Purchase Date</span>
+
             <input className="form-control" type="date" onChange={props.handleYarnPurchase}/>
         </div>
-        <div className="col-md-6">
-            <label className="form-label">No. of Available Skeins </label>
-            <input className="form-control" type="number" onChange={props.handleYarnSkeins}/>
-        </div>
-        <div className="col-md-6">
-            <label className="form-label">Purchase Location</label>
-            <input className="form-control" type="text" onChange={props.handleYarnStore}/>
-        </div>
+        
+             
         <div className="col-md-12">
-            <label className="form-label">Notes</label>
-            <textarea className="form-control" type="text" onChange={props.handleYarnNote}/>
+            <label className="form-label"></label>
+            <textarea className="form-control" type="text" onChange={props.handleYarnNote} placeholder="Notes"/>
         </div>
-        <div className="col-md-12">
-            <label className="form-label">Image URL</label>
-            <textarea className="form-control" type="url" onChange={props.handleYarnImage}/>
-        </div>
+
         <div className="col-12 m-3">
             <input className="btn btn-outline-primary" type="submit" value="Add New Yarn"/>
         </div>
